5271444c455177f72e96f9f0c4856de2231b9743
ruiz1994 <91515948+ruiz1994@users.noreply.github.com>
2022-11-02 20:50:34 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+let password1 = document.getElementById("password-1")
+let password2 = document.getElementById("password-2")
+let passwordLength = 15
+
+function getRandomCharacters() {
+    let randomIndex = Math.floor(Math.random() * characters.length)
+    return characters[randomIndex]
+
+}
+
+function generate() {
+    let randomPassword = ""
+    for (let i = 0; i < passwordLength; i++){
+        randomPassword += getRandomCharacters()
+    }
+    
+    return randomPassword
+}
+
+console.log(generate())
+
+function passwords() {
+    password1.textContent = generate()
+    password2.textContent = generate()
+ }
